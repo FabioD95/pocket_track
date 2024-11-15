1f2af9503b841b223d99bee4527c5bfe97a40e1a
FabioD95 <fabio.delrio95@gmail.com>
2024-11-15 16:47:30 +0100

    Enhance Redux store configuration: add middleware for redux-persist actions to improve state persistence

--- pocket_track_fe/src/store/index.ts
+++ pocket_track_fe/src/store/index.ts
@@ -1,6 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 import authReducer from '../store/authSlice';
 
@@ -13,6 +21,12 @@ const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
