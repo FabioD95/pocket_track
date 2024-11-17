12ffe1bb2a6d62fa79c44fff44c86e9dbebc0127
FabioD95 <fabio.delrio95@gmail.com>
2024-11-17 12:42:40 +0100

    Add NODE_ENV configuration and update test script: include NODE_ENV in package.json and conditionally connect to MongoDB based on environment

--- pocket_track_be/src/config/db.ts
+++ pocket_track_be/src/config/db.ts
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
-    console.log("MongoDB connected");
+    // console.log("NODE_ENV", process.env.NODE_ENV);
+    if (process.env.NODE_ENV === "dev") {
+      await mongoose.connect(process.env.MONGO_URI as string);
+      console.log("MongoDB connected");
+    }
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
