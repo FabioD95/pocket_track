addff242614f117ea480363ac7bcf10a7f3174bd
FabioD95 <fabio.delrio95@gmail.com>
2024-11-19 22:22:31 +0100

    Add createdAt field to Category and Tag models: include createdAt property with default value for better tracking of creation dates.

--- pocket_track_be/src/models/Category.ts
+++ pocket_track_be/src/models/Category.ts
@@ -2,10 +2,12 @@ import mongoose, { Document, Schema } from "mongoose";
 
 export interface ICategory extends Document {
   name: string;
+  createdAt: Date;
 }
 
 const CategorySchema: Schema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model<ICategory>("Category", CategorySchema);
--- pocket_track_be/src/models/Tag.ts
+++ pocket_track_be/src/models/Tag.ts
@@ -2,10 +2,12 @@ import mongoose, { Document, Schema } from "mongoose";
 
 export interface ITag extends Document {
   name: string;
+  createdAt: Date;
 }
 
 const TagSchema: Schema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model<ITag>("Tag", TagSchema);
